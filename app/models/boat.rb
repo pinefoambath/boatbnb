1ecf6574685768a5cfa95e175a75a084132af6e9
Viktor Emilov <kiwispooon@gmail.com>
2020-08-19 14:20:38 +0100

    test

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,12 +2,14 @@ class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+  has_one_attached :photo
 
   validates :name, presence: true
-  validates :length, presence: true
   validates :make, presence: true
-  validates :location_country, presence: true
-  validates :location_marina, presence: true
-  validates :sleeps, presence: true
+  validates :length, presence: true
+  validates :address, presence: true
   validates :daily_price, presence: true
+  validates :sleeps, presence: true
+  validates :year_built, presence: true
+  validates :boat_type, presence: true
 end

03fb67d1b739950ae2761c332f1a30cfafd5198f
Luke Norcross-Webb <lnw@me.com>
2020-08-19 14:47:57 +0100

    updated seed and model to make bookings dependent on boat

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,12 +24,11 @@ class BookingsController < ApplicationController
     @user = @booking.user
     @booking.destroy
     redirect_to root_path
-   end
+  end
 
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-
 end
--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,6 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   has_one_attached :photo
 
